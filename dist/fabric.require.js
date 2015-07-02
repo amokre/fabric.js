6cba211c0a387be14ffffcfea542d98d959a74d4
amokre <amokre@users.noreply.github.com>
2015-07-02 10:11:35 +0100

    Updated dist

--- dist/fabric.require.js
+++ dist/fabric.require.js
@@ -4101,7 +4101,7 @@ fabric.util.object.extend(fabric.StaticCanvas.prototype, {
             this._initClipping(options);
         },
         transform: function(ctx, fromLeft) {
-            if (this.group && this.canvas.preserveObjectStacking && this.group === this.canvas._activeGroup) {
+            if (this.group && this.canvas && this.canvas.preserveObjectStacking && this.group === this.canvas._activeGroup) {
                 this.group.transform(ctx);
             }
             var center = fromLeft ? this._getLeftTopCoords() : this.getCenterPoint();
--- src/shapes/object.class.js
+++ src/shapes/object.class.js
@@ -751,7 +751,8 @@
      * @param {Boolean} fromLeft When true, context is transformed to object's top/left corner. This is used when rendering text on Node
      */
     transform: function(ctx, fromLeft) {
-      if (this.group && this.canvas.preserveObjectStacking && this.group === this.canvas._activeGroup) {
+	  // (atWar) Added additional check for this.canvas
+      if (this.group && this.canvas && this.canvas.preserveObjectStacking && this.group === this.canvas._activeGroup) {
         this.group.transform(ctx);
       }
       var center = fromLeft ? this._getLeftTopCoords() : this.getCenterPoint();
